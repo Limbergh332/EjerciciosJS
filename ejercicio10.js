e6205067e0dd92656339e8e81c359326d3a00a05
Limbergh Morales Arellano <limbergh.moralesarellano@itsva.edu.mx>
2020-03-03 23:56:30 -0600

    modificado

---
+++ ejercicio10.js
@@ -0,0 +1,15 @@
+const numero=x=>{
+    var y = [];
+    var posicion=0;
+    var temporal;
+    for (let index = 0; index < x.length; index++) {
+        temporal = x[index] % 2;
+        if (temporal==0){
+            y[posicion]= x[index];
+            posicion=posicion+1;
+        }
+    }
+    console.log(y);
+}
+let a=[1,2,3,4,5,6];
+console.log(numero(a));
